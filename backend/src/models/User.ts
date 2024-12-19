39ca278a3b655972193b0ecd7e6ac5d51bad5dd5
Ted Shaffer <shaffer.family@gmail.com>
2024-12-19 07:30:26 -0800

    initial commit

---
+++ backend/src/models/User.ts
@@ -0,0 +1,19 @@
+import * as mongoose from 'mongoose';
+import { connection } from '../config';
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+  {
+    googleId: { type: String, required: true },
+    email: { type: String, required: true },
+    refreshToken: { type: String, required: true },
+  }
+);
+
+export const getUserModel = () => {
+  const userModel = connection.model('user', UserSchema);
+  return userModel;
+}
+
+export default UserSchema;

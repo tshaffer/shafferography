1fcd1a1b53a294bd373907604819e985dc01ef56
Ted Shaffer <shaffer.family@gmail.com>
2025-02-03 12:30:53 -0800

    added db scripts to backup collections

---
+++ mongoshScripts/backupCollections.js
@@ -0,0 +1,28 @@
+use pgPhotos;
+
+const collections = [
+  "mediaitems",
+  "deletedmediaitems"
+];
+
+const outputDir = "/Users/tedshaffer/Documents/MongoDBBackups/shafferography/backup-02-03-0-2025";
+
+const fs = require("fs");
+
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir, { recursive: true });
+}
+
+collections.forEach(collectionName => {
+  print(`Exporting collection: ${collectionName}`);
+
+  const data = db.getCollection(collectionName).find().toArray();
+
+  const outputFilePath = `${outputDir}/${collectionName}.json`;
+  fs.writeFileSync(outputFilePath, JSON.stringify(data, null, 2));
+
+  print(`Exported ${collectionName} to ${outputFilePath}`);
+});
+
+print("All collections exported successfully!");
+

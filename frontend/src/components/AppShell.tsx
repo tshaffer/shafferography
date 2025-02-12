285e065b160f62cc6a32f0b2fea778b905be75e5
Ted Shaffer <shaffer.family@gmail.com>
2025-02-12 05:21:39 -0800

    cleanup

--- frontend/src/components/AppShell.tsx
+++ frontend/src/components/AppShell.tsx
@@ -12,31 +12,6 @@ import TopNavigationBar from './TopNavigationBar';
 
 const drawerWidth = 240;
 
-type ViewMode = "grid" | "loupe" | "survey" | "fullscreen";
-
-const photos = [
-  { id: 1, src: "/images/J&M-2.jpg" },
-  { id: 2, src: "/images/J&M-3.jpg" },
-  { id: 3, src: "/images/J&M-4.jpg" },
-  { id: 4, src: "/images/J&M-5.jpg" },
-  { id: 5, src: "/images/J&M-10.jpg" },
-  { id: 6, src: "/images/J&M-11.jpg" },
-  { id: 7, src: "/images/J&M-12.jpg" },
-  { id: 8, src: "/images/J&M-13.jpg" },
-  { id: 9, src: "/images/J&M-14.jpg" },
-  { id: 11, src: "/images/J&M-15.jpg" },
-  { id: 12, src: "/images/J&M-16.jpg" },
-  { id: 13, src: "/images/J&M-17.jpg" },
-  { id: 14, src: "/images/J&M-18.jpg" },
-  { id: 15, src: "/images/J&M-19.jpg" },
-  { id: 16, src: "/images/J&M-20.jpg" },
-  { id: 17, src: "/images/J&M-21.jpg" },
-  { id: 18, src: "/images/J&M-22.jpg" },
-  { id: 19, src: "/images/J&M-23.jpg" },
-  { id: 20, src: "/images/J&M-24.jpg" },
-  { id: 21, src: "/images/J&M-25.jpg" },
-];
-
 declare module 'react' {
   interface InputHTMLAttributes<T> extends HTMLAttributes<T> {
     webkitdirectory?: string;
@@ -84,9 +59,6 @@ export interface AppShellProps {
 
 const AppShell = (props: AppShellProps) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [selectedPhotos, setSelectedPhotos] = useState<number[]>([]);
-  const [viewMode, setViewMode] = useState<ViewMode>("grid");
-  const [loupeIndex, setLoupeIndex] = useState<number>(0);
 
   React.useEffect(() => {
     props.onLoadMediaItems()
@@ -94,45 +66,6 @@ const AppShell = (props: AppShellProps) => {
         return props.onSetAppInitialized();
       });
   }, []);
-
-  // useEffect(() => {
-  //   if (viewMode === "loupe") {
-  //     if (selectedPhotos.length === 1) {
-  //       console.log('useEffect: selectedPhotos.length === 1');
-  //       setLoupeIndex(0);
-  //     } else if (selectedPhotos.length > 1) {
-  //       // const minSelectedPhoto = Math.min(...selectedPhotos);
-  //       // setLoupeIndex(minSelectedPhoto);
-  //       console.log('useEffect: selectedPhotos.length > 1');
-  //       setLoupeIndex(0);
-  //     }
-  //     setLoupeIndex(0);
-  //   }
-  // }, [viewMode, selectedPhotos]);
-
-  // useEffect(() => {
-  //   const handleKeyDown = (event: KeyboardEvent) => {
-  //     console.log('handleKeyDown', event.key);
-  //     console.log('loupeIndex', loupeIndex);
-  //     if (viewMode === "loupe") {
-  //       if (selectedPhotos.length > 1) {
-  //         if (event.key === "ArrowRight" && loupeIndex < selectedPhotos.length - 1) {
-  //           setLoupeIndex(loupeIndex + 1);
-  //         } else if (event.key === "ArrowLeft" && loupeIndex > 0) {
-  //           setLoupeIndex(loupeIndex - 1);
-  //         }
-  //       } else {
-  //         if (event.key === "ArrowRight" && loupeIndex < photos.length - 1) {
-  //           setLoupeIndex(loupeIndex + 1);
-  //         } else if (event.key === "ArrowLeft" && loupeIndex > 0) {
-  //           setLoupeIndex(loupeIndex - 1);
-  //         }
-  //       }
-  //     }
-  //   };
-  //   window.addEventListener("keydown", handleKeyDown);
-  //   return () => window.removeEventListener("keydown", handleKeyDown);
-  // }, [viewMode, loupeIndex, selectedPhotos]);
 
   const handleOpenSidebar = () => {
     setIsSidebarOpen(true);

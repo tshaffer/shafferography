568ee4f174d0de1843729e38cd03b0003f0ce0ad
Ted Shaffer <shaffer.family@gmail.com>
2025-02-19 05:48:26 -0800

    bloink

--- mongoshScripts/backupCollections.js
+++ mongoshScripts/backupCollections.js
@@ -14,7 +14,7 @@ const collections = [
 
 ];
 
-const outputDir = "/Users/tedshaffer/Documents/MongoDBBackups/shafferography/backup-02-17-0-2025";
+const outputDir = "/Users/tedshaffer/Documents/MongoDBBackups/shafferography/backup-02-19-0-2025";
 
 const fs = require("fs");
 

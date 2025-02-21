6fb3d3727e0b1b2bac625d32d5edda31a9d0749f
Ted Shaffer <shaffer.family@gmail.com>
2025-02-21 15:53:07 -0800

    restored code

--- frontend/src/selectors/selectedMediaItems.ts
+++ frontend/src/selectors/selectedMediaItems.ts
@@ -3,31 +3,36 @@ import {
   TedTaggerState
 } from '../types';
 
+import { getMediaItemById } from './mediaItems';
+
 import { createSelector } from 'reselect';
-import { getMediaItems } from './mediaItems';
 
 const EMPTY_ARRAY: string[] = [];
-
-export const getSelectedMediaItemIds = createSelector(
-  (state: TedTaggerState) => state.selectionsState?.selectedMediaItemIds || EMPTY_ARRAY,
-  (selectedMediaItemIds) => selectedMediaItemIds
-);
-
-export const getSelectedMediaItems = createSelector(
-  [getSelectedMediaItemIds, getMediaItems],
-  (selectedMediaItemIds, mediaItems) => {
-    const selectedSet = new Set(selectedMediaItemIds); // Efficient lookup
-    return mediaItems.filter(item => selectedSet.has(item.uniqueId));
-  }
-);
 
 export const getSelectedMediaItemsCount = (state: TedTaggerState): number => {
   return state.selectionsState.selectedMediaItemIds.length;
 };
 
+export const getSelectedMediaItems = (state: TedTaggerState): MediaItem[] => {
+  const selectedMediaItemIds: string[] = getSelectedMediaItemIds(state);
+  const selectedMediaItems: MediaItem[] = [];
+  for (const selectedMediaItemId of selectedMediaItemIds) {
+    const selectedMediaItem = getMediaItemById(state, selectedMediaItemId);
+    if (selectedMediaItem) {
+      selectedMediaItems.push(selectedMediaItem);
+    }
+  }
+  return selectedMediaItems;
+};
+
 export const getLastClickedId = (state: TedTaggerState): string | null => {
   return state.selectionsState.lastClickedId;
 };
+
+export const getSelectedMediaItemIds = createSelector(
+  (state: TedTaggerState) => state.selectionsState?.selectedMediaItemIds || EMPTY_ARRAY,
+  (selectedMediaItemIds) => selectedMediaItemIds
+);
 
 export const isMediaItemSelected = createSelector(
   [getSelectedMediaItemIds, (state, mediaItem) => mediaItem.uniqueId],
@@ -36,5 +41,4 @@ export const isMediaItemSelected = createSelector(
     return result;
   }
 );
-
 

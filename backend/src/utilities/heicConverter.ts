6aa9c2761049566e893bb976c0ea6a1e1d0e3f55
Ted Shaffer <shaffer.family@gmail.com>
2025-03-04 06:07:21 -0800

    tried to use libheif to open problematic heic files.

--- backend/src/utilities/heicConverter.ts
+++ backend/src/utilities/heicConverter.ts
@@ -2,12 +2,13 @@ const { promisify } = require('util');
 const fs = require('fs');
 import convert from 'heic-convert';
 import { copyExifTags } from '../utilities';
+const libheif = require('libheif-js');
 
 export const convertHEICFileToJPEGWithEXIF = async (inputFilePath: string, outputFilePath: string): Promise<void> => {
   try {
     console.log(`convertHEICFileToJPEGWithEXIF: ${inputFilePath} to ${outputFilePath}`);
     await convertHEICFileToJPEG(inputFilePath, outputFilePath);
-    copyExifTags(inputFilePath, outputFilePath, true);
+    // copyExifTags(inputFilePath, outputFilePath, true);
   }
   catch (error) {
     console.error('Error in convertHEICFileToJPEGWithEXIF:', error);
@@ -16,11 +17,17 @@ export const convertHEICFileToJPEGWithEXIF = async (inputFilePath: string, outpu
 }
 
 async function convertHEICFileToJPEG(inputFilePath: string, outputFilePath: string): Promise<void> {
-  const inputBuffer = await promisify(fs.readFile)(inputFilePath);
-  const outputBuffer = await convert({
-    buffer: inputBuffer, // the HEIC file buffer
-    format: 'JPEG',      // output format
-    quality: 1           // the jpeg compression quality, between 0 and 1
-  });
-  await promisify(fs.writeFile)(outputFilePath, outputBuffer);
+  const file = fs.readFileSync(inputFilePath);
+  const decoder = new libheif.HeifDecoder();
+  const data = await decoder.decode(file);
+  console.log('Decoded HEIF data:', data);
+
+  // const inputBuffer = await promisify(fs.readFile)(inputFilePath);
+  // await decodeHEIF(inputBuffer);
+  // const outputBuffer = await convert({
+  //   buffer: inputBuffer, // the HEIC file buffer
+  //   format: 'JPEG',      // output format
+  //   quality: 1           // the jpeg compression quality, between 0 and 1
+  // });
+  // await promisify(fs.writeFile)(outputFilePath, outputBuffer);
 }

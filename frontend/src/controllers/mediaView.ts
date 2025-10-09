cb7707648728047f064f0495a430ca9863939ee5
Ted Shaffer <shaffer.family@gmail.com>
2025-10-09 08:12:11 -0700

    persist to backend - needs testing. it might work.

--- frontend/src/controllers/mediaView.ts
+++ frontend/src/controllers/mediaView.ts
@@ -1,5 +1,7 @@
+import axios from "axios";
 import { setViewVariant } from "../models";
 import { getViewVariant } from "../selectors/mediaView";
+import { getServerUrl, apiUrlFragment } from "../types";
 
 export const persistPreferredVariant = (
   mediaItemId: string,
@@ -14,4 +16,41 @@ export const persistPreferredVariant = (
   //    - if kind === 'original' → set preferredDerivativeId = null
   //    - if derivative → set preferredDerivativeId = payload.derivativeId
   // 2) Dispatch a Redux action to update the media item in state (so UI reflects the change immediately).
+
+  /*
+  
+  persistPreferredVariant
+    const path = getServerUrl() + apiUrlFragment + mediaItemId + "/derivatives";
+    app.put('/api/v1/:id/preferred/:derivativeId', putPreferred);
+    // router.put("/:id/preferred/:derivativeId", async (req: Request, res: Response) => {
+
+  cropMediaItem
+    const body = { cropData, markPreferred: true };
+    const path = getServerUrl() + apiUrlFragment + mediaItemId + "/derivatives";
+    app.post('/api/v1/:mediaItemId/derivatives', generateDerivativeEndpoint);
+
+  generateDerivativeEndpoint
+    const { mediaItemId } = req.params;
+    const body = req.body;
+    body.cropData
+  
+  */
+  const derivativeId = payload.kind === 'derivative' ? payload.derivativeId : mediaItemId;
+  const path = getServerUrl() + apiUrlFragment + mediaItemId + "/preferred/" + derivativeId;
+
+
+  // const path = getServerUrl() + apiUrlFragment + 'album-tree';
+  // const body = {
+  //   mediaItemId,
+  //   payload,
+  // };
+  return axios.put(path)
+    .then((response: any) => {
+      return Promise.resolve();
+    }).catch((error) => {
+      console.log('error');
+      console.log(error);
+      return '';
+    });
+
 };

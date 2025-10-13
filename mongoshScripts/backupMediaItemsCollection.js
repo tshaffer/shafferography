971da06d74396230cadef092c41d1486b898b84c
Ted Shaffer <shaffer.family@gmail.com>
2025-10-13 09:01:46 -0700

    new script to specifically backup the mediaItems collection

---
+++ mongoshScripts/backupMediaItemsCollection.js
@@ -0,0 +1,27 @@
+use pgPhotos;
+
+const collections = [
+  "mediaitems",
+];
+
+const outputDir = "/Users/tedshaffer/Documents/MongoDBBackups/shafferography/preSchemaUpdate-mediaItems-backup-10-13-1";
+
+const fs = require("fs");
+
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir, { recursive: true });
+}
+
+collections.forEach(collectionName => {
+  print(`Exporting collection: ${collectionName}`);
+
+  const data = db.getCollection(collectionName).find().toArray();
+
+  const outputFilePath = `${outputDir}/${collectionName}.json`;
+  fs.writeFileSync(outputFilePath, JSON.stringify(data, null, 2));
+
+  print(`Exported ${collectionName} to ${outputFilePath}`);
+});
+
+print("All collections exported successfully!");
+
